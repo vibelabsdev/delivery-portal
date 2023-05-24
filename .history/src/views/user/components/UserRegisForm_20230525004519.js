61cfaa75866a813c7f896a35dd00d23a392ee7e3
anhxuanpham@hotmail.com <anhxuanpham@hotmail.com>
2023-05-25 01:03:36 +0700

    _convert_timestamp_to_date_time_

--- .history/src/views/user/components/UserRegisForm_20230525004519.js
+++ .history/src/views/user/components/UserRegisForm_20230525004519.js
@@ -18,7 +18,7 @@ function validateEmail(value) {
     if (!value) {
         error = 'Required'
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-        error = 'Địa chỉ email không đúng định dạng!'
+        error = 'Invalid email address'
     }
     return error
 }
