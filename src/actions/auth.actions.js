c08d0e42d2de5933c63e2fde173627e9fc5c5a7c
anhxuanpham@hotmail.com <anhxuanpham@hotmail.com>
2023-05-30 00:18:25 +0700

    __translate_register_form

--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -1,31 +1,25 @@
 import { authService } from "services";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import useQuery from "utils/hooks/useQuery";
 
 export const authLogin = async (dataReq) => {
-  const navigate = useNavigate()
-  const query = useQuery()
+  const navigate = useNavigate();
+  const query = useQuery();
 
   if (dataReq) {
-    console.log('----authLogin has work---', dataReq)
+    console.log("----authLogin has work---", dataReq);
     try {
-      
       const response = await authService.login(dataReq);
       const data = await response.json();
-      console.log('----data----', data)
+      console.log("----data----", data);
       if (data) {
-        const redirectUrl = query.get(REDIRECT_URL_KEY)
-        console.log('-------redirectUrl----', redirectUrl)
-        navigate(
-            redirectUrl ? redirectUrl : appConfig.authenticatedEntryPath
-        )
+        const redirectUrl = query.get(REDIRECT_URL_KEY);
+        console.log("-------redirectUrl----", redirectUrl);
+        navigate(redirectUrl ? redirectUrl : appConfig.authenticatedEntryPath);
       }
-      return data
+      return data;
     } catch (error) {
       // dispatch(failure(error));
-      
     }
   }
 };
-
-
--- src/views/user/index.js
+++ src/views/user/index.js
@@ -1,30 +1,33 @@
-import React, { useEffect } from 'react'
-import { AdaptableCard } from 'components/shared'
+import React, { useEffect } from "react";
+import { AdaptableCard } from "components/shared";
 
-import { Link, useLocation } from 'react-router-dom'
-import UserRegisForm from './components/UserRegisForm'
+import { Link, useLocation } from "react-router-dom";
+import UserRegisForm from "./components/UserRegisForm";
 
 // injectReducer('store', reducer)
 
 const StoreCreate = () => {
-    const {state} = useLocation();
-    const { store_name, store_id } = state; // Read values passed on state
+  const { state } = useLocation();
+  const { store_name, store_id } = state; // Read values passed on state
 
-    return (
-        <AdaptableCard className="h-full" bodyClass="h-full">
-            <div className="lg:flex items-center justify-between mb-4">
-                <h3 className="mb-4 lg:mb-0">User Register - { store_name? store_name : ''}</h3>
-                <Link
-                    // activeClass="text-gray-900 dark:text-gray-50"
-                    className="cursor-pointer block transform transition-colors duration-200 py-2 hover:text-gray-900 dark:hover:text-gray-100"
-                    to="/delivery-store"
-                >
-                    Back
-                </Link>
-            </div>
-            <UserRegisForm state={state} />
-        </AdaptableCard>
-    )
-}
+  return (
+    <AdaptableCard className="h-full" bodyClass="h-full">
+      <div className="lg:flex items-center justify-between mb-4">
+        <h3 className="mb-4 lg:mb-0">
+          {" "}
+          Tạo tài khoản - {store_name ? store_name : ""}{" "}
+        </h3>{" "}
+        <Link
+          // activeClass="text-gray-900 dark:text-gray-50"
+          className="cursor-pointer block transform transition-colors duration-200 py-2 hover:text-gray-900 dark:hover:text-gray-100"
+          to="/delivery-store"
+        >
+          Back{" "}
+        </Link>{" "}
+      </div>{" "}
+      <UserRegisForm state={state} />{" "}
+    </AdaptableCard>
+  );
+};
 
-export default StoreCreate
+export default StoreCreate;
