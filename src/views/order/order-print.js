80d75419d06592a87cb1635af423b1118832d443
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2023-09-30 01:48:54 +0700

    _convert_order_type_

--- src/views/order/order-print.js
+++ src/views/order/order-print.js
@@ -8,6 +8,17 @@ const FormatPrice = (price) => {
 }
 
 export const OrderPrint = React.forwardRef(({data}, ref) => {
+
+    const orderTypeConvert = {
+        normal: {
+            label: 'Giao thường',
+            class: 'text-blue-500 dark:bg-emerald-500/20 dark:text-emerald-100',
+        },
+        express: {
+            label: 'Giao nhanh',
+            class: 'text-yellow-500 dark:bg-emerald-500/20 dark:text-emerald-100',
+        },
+    }
     
     return (
       <div ref={ref} className='flex-col justify-center items-center mt-[24px] py-4 flex'>
@@ -28,7 +39,7 @@ export const OrderPrint = React.forwardRef(({data}, ref) => {
                 <p>Tên: {data.cust_name}</p>
                 <p>Số điện thoại: {data.cust_phone}</p>
                 <p>Địa chỉ: {data.address}</p>
-                <p>Loại giao: {data.order_type}</p>
+                <p>Loại giao: {orderTypeConvert[data.order_type]?.label}</p>
             </div>
             <div className='flex flex-col'>
                 <p>Tiền hàng: {FormatPrice(data.total_amount - data.fee_ship)}</p>
